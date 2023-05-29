edc4aa9263d59cadc0d440503497b85235a5de37
PEMessage <1165739182@qq.com>
2023-05-29 22:40:12 +0800

    Update

--- private_dot_config/pem/shell/vless/vless.sh
+++ private_dot_config/pem/shell/vless/vless.sh
@@ -1,3 +1,5 @@
 
-alias vless='~/.config/pem/shell/vless/vim-less.sh'
+alias vless='~/.config/pem/shell/vless/vless-script.sh'
+
+export MANPAGER="/bin/sh -c \"col -b | vim -c 'set ft=man ts=8 nomod nolist nonu noma' -\""
 
