cb9d59f5291892deb762d7c32c5af8070eea3d38
PEMessage <1165739182@qq.com>
2023-06-12 01:29:34 +0800

    Update

--- private_dot_config/pem/shell/vless/vless.sh
+++ private_dot_config/pem/shell/vless/vless.sh
@@ -1,6 +1,5 @@
-
-
-
 # export MANPAGER="/bin/sh -c \"col -b | vim -c 'set ft=man ts=8 nomod nolist nonu noma' -\""
-alias vman='MANPAGER=nvim man'
+alias vman="MANPAGER='vim +MANPAGER \"+set nonumber\" -'  man"
+
+
 
