0f12b8232228601c318fe6f6184d1b0f6867d29e
PEMessage <1165739182@qq.com>
2023-06-12 23:15:39 +0800

    Update

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
+#! /usr/bin/bash
+
 pwd
 ls -al
+
+cp -f ./linux/dot_vimrc ./windows/dot_vimrc 
+
+mkdir -p ./windows/AppData/Local/nvim
+cp -f ./linux/private_dot_config/nvim/init.lua  ./windows/AppData/Local/nvim/init.lua
