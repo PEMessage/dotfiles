407eda4f84040a88abdf47f1e816dd05cdf6a91c
PEMessage <1165739182@qq.com>
2023-06-13 23:45:12 +0800

    Update

--- private_dot_config/pem/shell/vless/vless.sh
+++ private_dot_config/pem/shell/vless/vless.sh
@@ -1,5 +1,31 @@
 # export MANPAGER="/bin/sh -c \"col -b | vim -c 'set ft=man ts=8 nomod nolist nonu noma' -\""
 alias vman="MANPAGER='vim +MANPAGER \"+set nonumber\" -'  man"
 
+# alias vvman="nvim -c \"lua require('telescope.builtin').man_pages({sections={'ALL'}})\""
+
+vvman(){
+    local temp="
+
+       require('telescope.builtin').man_pages({
+         sections={'ALL'},
+         attach_mappings = function(_, map)
+            map(
+              {'i'},
+              '<Enter>',
+              function(...)
+                return require('telescope.actions').select_tab(...)
+              end
+            )
+            return true
+         end,
+       })
+
+    "
+
+    nvim -c "lua   $temp      " 
+
+}
+
+
 
 
