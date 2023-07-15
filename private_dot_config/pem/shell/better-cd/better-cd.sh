3011274578f003dedba619811405dab67381fe7f
PEMessage <1165739182@qq.com>
2023-07-15 23:01:04 +0800

    Update time: 2023-07-15 15:01

--- private_dot_config/pem/shell/better-cd/better-cd.sh
+++ private_dot_config/pem/shell/better-cd/better-cd.sh
@@ -5,18 +5,22 @@ bcd()
 {
     local location_path
     if [ "$1" = '-' ]; then
-        location_path=`cat -`
+        location_path=$(</dev/stdin)
     else
         location_path="$1"
     fi
 
     local pathtype
-    pathtype=$( file $location_path | cut -d ' ' -f 2 )
+    pathtype=$( file "$location_path" | cut -d ' ' -f 2 )
+
+    # echo $pathtype
+    
     if [ "$pathtype" = "directory" ] ; then
         cd "$location_path"
-        return
+        return 0
     else
-        cd `dirname $location_path`
-        return
+        location_path=`dirname "$location_path"`
+        cd "$location_path" > /dev/null 2&>1 
+        return 0
     fi
 }
