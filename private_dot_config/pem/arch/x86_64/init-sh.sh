5521543632d52499ead642051357dcaf7027d10d
PEMessage <1165739182@qq.com>
2023-07-21 02:22:43 +0800

    Clean: arch bin struct

--- private_dot_config/pem/arch/x86_64/init-sh.sh
+++ private_dot_config/pem/arch/x86_64/init-sh.sh
@@ -1,11 +1,11 @@
 # set PATH so it includes user's private bin if it exists
 
-if [ -d "$PEMHOME/arch/x86_64/bin" ]; then
-    export PATH="$PATH:$PEMHOME/arch/x86_64/bin"
-fi
+# if [ -d "$PEMHOME/arch/x86_64/bin" ]; then
+#     export PATH="$PATH:$PEMHOME/arch/x86_64/bin"
+# fi
 
 for d in "$PEMHOME"/arch/x86_64/bin/*.d; do 
-    echo $d
+    export PATH="$PATH:$d"
 done
 
 export PATH
