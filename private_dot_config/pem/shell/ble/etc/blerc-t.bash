a0b099c4a1959e9e2570ef4dfe085b0da61ce0b4
PEMessage <1165739182@qq.com>
2023-08-28 00:36:59 +0800

    Update time: 2023-08-27 16:36

---
+++ private_dot_config/pem/shell/ble/etc/blerc-t.bash
@@ -0,0 +1,17 @@
+bleopt history_share=1
+
+# bleopt complete_auto_complete=
+bleopt complete_menu_complete=
+bleopt complete_menu_filter=
+
+bleopt complete_ambiguous=
+bleopt complete_contract_function_names=
+bleopt complete_allow_reduction=
+bleopt complete_auto_history=1
+
+# Color
+bleopt highlight_syntax=
+bleopt highlight_filename=
+bleopt highlight_variable=
+
+ble-face auto_complete='fg=240'
