a7264e5d4f875cdcd07d61b948e403c4877b96c7
PEMessage <1165739182@qq.com>
2024-08-01 11:38:55 +0800

    wsl: add px

---
+++ private_dot_config/pem/shell/wsl/executable_px.sh
@@ -0,0 +1,27 @@
+PREV_hostip=
+PREV_https_proxy=
+PREV_http_proxy=
+
+px() {
+    PREV_hostip="$hostip"
+    PREV_https_proxy="$https_proxy"
+    PREV_http_proxy="$http_proxy"
+
+    export hostip=$(cat /etc/resolv.conf |grep -oP '(?<=nameserver\ ).*')
+    export https_proxy="http://${hostip}:7890"
+    export http_proxy="http://${hostip}:7890"
+
+    echo export hostip=$(cat /etc/resolv.conf |grep -oP '(?<=nameserver\ ).*')
+    echo export https_proxy="http://${hostip}:7890"
+    echo export http_proxy="http://${hostip}:7890"
+}
+
+unpx() {
+    echo export hostip="$PREV_hostip"
+    echo export https_proxy="$PREV_https_proxy"
+    echo export http_proxy="$PREV_http_proxy"
+    export hostip="$PREV_hostip"
+    export https_proxy="$PREV_https_proxy"
+    export http_proxy="$PREV_http_proxy"
+}
+
