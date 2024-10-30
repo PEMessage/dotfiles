a7585ac54fd5851cbde9bc2d264eb32ad15a4fc8
PEMessage <1165739182@qq.com>
2024-10-30 20:00:48 +0800

    nvim: update config

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -699,6 +699,35 @@ require("lazy").setup({
     {
         'Bekaboo/dropbar.nvim',
         opts = {
+            sources = {
+                path = {
+                    preview = false,
+                }
+            },
+            bar = {
+                sources = function(buf, _)
+                    local sources = require('dropbar.sources')
+                    local utils = require('dropbar.utils')
+                    if vim.bo[buf].ft == 'markdown' then
+                        return {
+                            sources.path,
+                            sources.markdown,
+                        }
+                    end
+                    if vim.bo[buf].buftype == 'terminal' then
+                        return {
+                            sources.terminal,
+                        }
+                    end
+                    return {
+                        sources.path,
+                        utils.source.fallback({
+                            sources.lsp,
+                            sources.treesitter,
+                        }),
+                    }
+                end
+            },
             icons = {
                 ui  = {
                     bar = {
