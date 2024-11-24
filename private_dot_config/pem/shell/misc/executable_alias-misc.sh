94f92055abb4661b38af7ec760c6b9afa9f928c3
pemessage <1165739182@qq.com>
2024-11-24 23:02:26 +0800

    vim: add lvim(last vim open)

--- private_dot_config/pem/shell/misc/executable_alias-misc.sh
+++ private_dot_config/pem/shell/misc/executable_alias-misc.sh
@@ -2,6 +2,7 @@ alias ffapt='ffpm i'
 alias rsync='rsync --progress'
 alias s="git status --short --branch"
 alias vit='vim $(command ls -tp | grep -v / |  head -n1)'
+alias lvim='vim -c "normal '\''0"'
 # alias ttt='command ls -tp | head -n1'
 ttt() {
     if [ -n "$1" ] ; then
