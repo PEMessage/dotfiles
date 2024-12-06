fce0de0fb2e06edec5898fb38a8605e60ae1eeeb
PEMessage <1165739182@qq.com>
2024-12-06 22:44:52 +0800

    px: move to px2

---
+++ private_dot_config/pem/shell/wsl/executable_px2.sh
@@ -0,0 +1,45 @@
+# Function to set up proxy settings
+
+px2dns() {
+    local hostip="$(cat /etc/resolv.conf |grep -oP '(?<=nameserver\ ).*')"
+    __px2_defconfig set $hostip:7890
+}
+
+alias px=px2dns
+
+px2() {
+    __px2_defconfig set "$1:$2"
+}
+
+unpx() {
+    __px2_defconfig unset
+}
+
+
+__px2_defconfig() {
+    __px2_set_or_unset $1 http_proxy "http://$2"
+    __px2_set_or_unset $1 https_proxy "http://$2"
+    # you need a mix-port support
+    __px2_set_or_unset $1 sock5h_proxy "sock5h://$2"
+}
+
+__px2_set_or_unset() {
+    cmd="eval"
+    if [ "$1" = "set"  ] ; then
+        echo "export $2=\"$3\""
+        $cmd "export $2=\"$3\""
+
+        echo "export $(echo $2 | tr '[:lower:]' '[:upper:]')=\"$3\""
+        $cmd "export $(echo $2 | tr '[:lower:]' '[:upper:]')=\"$3\""
+
+        return
+    elif [ "$1" = "unset" ] ; then
+        echo "unset $2"
+        $cmd "unset $2"
+
+        echo "unset $(echo $2 | tr '[:lower:]' '[:upper:]')"
+        $cmd "unset $(echo $2 | tr '[:lower:]' '[:upper:]')"
+
+        return
+    fi
+}
