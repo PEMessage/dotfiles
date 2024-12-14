53c99f32fd89a42a0e76d67c68808f4f069b7df5
PEMessage <1165739182@qq.com>
2024-12-15 00:44:00 +0800

    init: add pwsh ps1

---
+++ readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+# Alias and Basic Setting
+# # =========================================
+
+    Remove-Item alias:curl
+    Set-Alias -Name l -Value ls
+
+# PSReadline
+# # =========================================
+
+    Set-PSReadLineOption -EditMode Emacs
+
+
