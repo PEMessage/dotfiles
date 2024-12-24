45a0a6f345676ba734565c3e0e24720b6a92b294
PEMessage <1165739182@qq.com>
2024-12-24 21:04:39 +0800

    alias: add msudo

--- private_dot_config/pem/shell/misc/executable_alias-misc.sh
+++ private_dot_config/pem/shell/misc/executable_alias-misc.sh
@@ -4,6 +4,7 @@ alias s="git status --short --branch"
 alias vit='vim $(command ls -tp | grep -v / |  head -n1)'
 alias lvim='vim -c "normal '\''0"'
 alias lvi='vim -c "normal '\''0"'
+alias msudo="sudo env \"PATH=\$PATH\""
 
 # alias ttt='command ls -tp | head -n1'
 ttt() {
