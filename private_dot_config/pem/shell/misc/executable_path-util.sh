3ac84c6abd657e50cf0716d743a043c784a1eedf
PEMessage <1165739182@qq.com>
2025-02-13 23:56:02 +0800

    path-util: also print cmd in a2p

--- private_dot_config/pem/shell/misc/executable_path-util.sh
+++ private_dot_config/pem/shell/misc/executable_path-util.sh
@@ -3,7 +3,11 @@
 
 # Add current dir to PATH
 a2p() {
+    echo "Runing:"
+    echo "export PATH=\"$(pwd):\$PATH\""
     export PATH="$(pwd):$PATH"
+    echo
+    echo "Current Path:"
     echo $PATH | tr ':' '\n' | grep -v '^$' | head -n5
     echo '...'
 }
