d9947fffd3daafe71f45119f9fec03645ae752ef
PEMessage <1165739182@qq.com>
2025-03-11 21:01:58 +0800

    path-util: allow a2p adding path from arguement

--- private_dot_config/pem/shell/misc/executable_path-util.sh
+++ private_dot_config/pem/shell/misc/executable_path-util.sh
@@ -3,9 +3,15 @@
 
 # Add current dir to PATH
 a2p() {
-    echo "Runing:"
-    echo "export PATH=\"$(pwd):\$PATH\""
-    export PATH="$(pwd):$PATH"
+    if [ "$1" != "" ] ; then 
+        echo "Runing:"
+        echo "export PATH=\"$(readlink -f "$1"):\$PATH\""
+        export PATH="$(readlink -f "$1"):$PATH"
+    else 
+        echo "Runing:"
+        echo "export PATH=\"$(pwd):\$PATH\""
+        export PATH="$(pwd):$PATH"
+    fi
     echo
     echo "Current Path:"
     echo $PATH | tr ':' '\n' | grep -v '^$' | head -n5
