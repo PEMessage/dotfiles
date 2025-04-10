01c06bcba5d348c8a5d577b933cd1b06df4fa5ff
PEMessage <1165739182@qq.com>
2025-04-11 01:31:25 +0800

    nvim: update to 0.11

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -870,7 +870,16 @@ require("lazy").setup({
                     -- }
                 -- }
             }
-        }
+        },
+        config = function(_,opts)
+            vim.cmd [[ highlight! link WinBar StatusLine ]]
+            require('dropbar').setup(opts)
+            local dropbar_api = require('dropbar.api')
+            vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+            vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+            vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+        end
+
         -- optional, but required for fuzzy finder support
         -- dependencies = {
             -- 'nvim-telescope/telescope-fzf-native.nvim'
