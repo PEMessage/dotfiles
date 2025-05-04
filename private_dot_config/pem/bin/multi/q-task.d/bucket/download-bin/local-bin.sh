1049541eabd13e2975954f7fe6e920a1b0f33e65
PEMessage <1165739182@qq.com>
2025-05-05 00:10:36 +0800

    q-task: add local-bin.sh

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/download-bin/local-bin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+runcmd() {
+    echo "Runing: $*"
+    "$@"
+}
+
+if [ ! -f aichat ] ; then
+    runcmd github-download sigoden/aichat \
+        'aichat-.*-x86_64-unknown-linux-musl.tar.gz' \
+        aichat.tar.gz
+    runcmd q-extract -x aichat.tar.gz aichar:aichat
+    runcmd rm -rf aichat.tar.gz
+fi
