c4a02384957a87fe1fe9b08bc00b3c56531d8f83
PEMessage <1165739182@qq.com>
2025-05-26 23:11:34 +0800

    PS1: add Delete-LearnMoreAboutThisImage.ps1

---
+++ readonly_Documents/Script/PS1/Delete-LearnMoreAboutThisImage.ps1
@@ -0,0 +1,2 @@
+#https://www.bilibili.com/opus/906089351864123442
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{2cc5ca98-6485-489a-920e-b3e88a6ccce3}" /t REG_DWORD /d 1 /f
