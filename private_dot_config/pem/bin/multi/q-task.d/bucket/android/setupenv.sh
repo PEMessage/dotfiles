ae95dde23a76503fd4a30ba958a2ce569bbfe5f3
PEMessage <1165739182@qq.com>
2025-05-28 01:19:06 +0800

    q-task: add android/setupenv.sh

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/android/setupenv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+
+COMMANDLINETOOLS_URL="https://dl.google.com/android/repository/commandlinetools-linux-13114758_latest.zip"
+setup_cmdline-tools() {
+    (
+        if [  -d "cmdline-tools" ] ; then
+            return 
+        fi
+
+        if [ ! -f "commandlinetools.zip" ] ; then
+            curl --progress-bar \
+                "$COMMANDLINETOOLS_URL" \
+                --output commandlinetools.zip
+        fi
+        unzip commandlinetools.zip
+    )
+}
