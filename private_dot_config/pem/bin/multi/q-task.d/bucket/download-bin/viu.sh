680379b5939d103287b6a07ec9a04352cf5ac632
PEMessage <1165739182@qq.com>
2025-06-02 00:34:12 +0800

    q-task: download-bin add viu.sh

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/download-bin/viu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+runcmd() {
+    echo "Runing: $*"
+    "$@"
+}
+
+if [ ! -f viu ] ; then
+    runcmd github-download atanunq/viu \
+        'viu-x86_64-unknown-linux-musl' \
+        viu
+    chmod a+x viu
+fi
