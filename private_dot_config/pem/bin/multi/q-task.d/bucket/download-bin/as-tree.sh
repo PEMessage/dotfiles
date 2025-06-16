5ead2d99d5f9f4f6711c2674b247ea76117d16fe
PEMessage <1165739182@qq.com>
2025-06-17 00:20:48 +0800

    q-task: add as-tree.sh

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/download-bin/as-tree.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+runcmd() {
+    echo "Runing: $*"
+    "$@"
+}
+
+if [ ! -f as-tree ] ; then
+    runcmd github-download jez/as-tree \
+        'as-tree-.*-linux.zip' \
+        as-tree.zip
+    runcmd q-extract -x as-tree.zip as-tree:as-tree
+    runcmd rm -rf as-tree.zip
+fi
