8a4e950875e96e7718a312ca185e7178bea1d513
PEMessage <1165739182@qq.com>
2025-06-20 01:13:51 +0800

    q-task: add lazygit

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/download-bin/lazygit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+runcmd() {
+    echo "Runing: $*"
+    "$@"
+}
+
+if [ ! -f lazygit ] ; then
+    runcmd github-download jesseduffield/lazygit \
+        'lazygit_.*_Linux_x86_64.tar.gz' \
+        lazygit.tar.gz
+    runcmd q-extract -x lazygit.tar.gz lazygit:lazygit
+    runcmd rm -rf lazygit.tar.gz
+fi
