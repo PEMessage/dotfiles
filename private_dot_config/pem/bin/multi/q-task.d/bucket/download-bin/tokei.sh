4a46e12ea0b0d594bca9f76b030ecd2f85599c19
PEMessage <1165739182@qq.com>
2025-07-02 00:24:17 +0800

    q-task: add tokei.sh

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/download-bin/tokei.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+runcmd() {
+    echo "Runing: $*"
+    "$@"
+}
+
+if [ ! -f tokei ] ; then
+    runcmd github-download XAMPPRocky/tokei -v v13.0.0-alpha.0 \
+        'tokei-x86_64-unknown-linux-musl.tar.gz' \
+        tokei.tar.gz
+    runcmd q-extract -x tokei.tar.gz tokei:tokei
+    runcmd rm -rf tokei.tar.gz
+fi
