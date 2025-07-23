1039c05df46f55be1274d9bd2d21412d97602a60
PEMessage <1165739182@qq.com>
2025-07-24 00:50:56 +0800

    q-task: add astgrep/insert-braces-in-if.sh

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/astgrep/insert-braces-in-if.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+INLINE_RULE="$(cat <<'EOF'
+id: insert-braces-in-if
+language: c
+rule:
+  kind: return_statement
+  pattern: return $$$X ;
+  inside:
+    any:
+      - kind: if_statement
+        not:
+          has:
+            kind: compound_statement
+      - kind: else_clause
+        not:
+          has:
+            kind: compound_statement
+fix:
+  "{ return $$$X; }"
+message: Insert braces around single-line if statement
+EOF
+)"
+
+# echo "$INLINE_RULE"
+ast-grep scan --inline-rules "$INLINE_RULE" "$@"
