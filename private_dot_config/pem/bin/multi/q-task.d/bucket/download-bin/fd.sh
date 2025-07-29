d4aff4f67060d61ec3f071b2c60d6768cf89ef46
PEMessage <1165739182@qq.com>
2025-07-30 00:25:36 +0800

    q-task: add fd

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/download-bin/fd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+runcmd() {
+    echo "Runing: $*"
+    "$@"
+}
+
+if [ ! -f fd ] ; then
+    runcmd github-download sharkdp/fd \
+        'fd-.*-x86_64-.*-linux-musl.tar.gz' \
+        fd.tar.gz
+    filename="$(q-extract -l fd.tar.gz | grep '/fd$')"
+    runcmd q-extract -x fd.tar.gz "$filename:fd"
+    runcmd rm -rf fd.tar.gz
+fi
