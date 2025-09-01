6b940ee88e7892075d2525a3ec889ef6303af036
PEMessage <1165739182@qq.com>
2025-09-02 00:42:03 +0800

    nvim: tweak telescope-fzf-native build cmd for old cmake

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -867,7 +867,7 @@ require("lazy").setup({
     -- -------------------------------------------
     {
         'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
+        build = 'cmake -S . -B build -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
     },
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.8',
@@ -1487,6 +1487,7 @@ require("lazy").setup({
                     },
                     settings = {
                         java = {
+                            contentProvider = { preferred = 'fernflower' },
                             inlayhints = {
                                 parameterNames = { enabled = true },
                             },
