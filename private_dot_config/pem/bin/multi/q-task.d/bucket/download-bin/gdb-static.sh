45c6517eeea4b5eaf8b5276260cae7bb51d399b4
PEMessage <1165739182@qq.com>
2025-09-23 23:56:18 +0800

    q-task: download-bin add gdb-static.sh

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/download-bin/gdb-static.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+runcmd() {
+    echo "Runing: $*"
+    "$@"
+}
+
+if [ ! -f gdb-static ] ; then
+    runcmd github-download guyush1/gdb-static \
+        'gdb-static-full-x86_64.tar.gz' \
+        gdb-static.tar.gz
+    mkdir gdb-static
+    mv gdb-static.tar.gz gdb-static
+    cd gdb-static
+    q-extract gdb-static.tar.gz
+    rm gdb-static.tar.gz
+
+    mv gdb gdb-static
+fi
