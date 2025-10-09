e3c4e13875875b41c3eac32ea6b390dbaa77607a
PEMessage <1165739182@qq.com>
2025-10-10 00:43:37 +0800

    q-task: add astgrep/find-def.sh

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/astgrep/find-def.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+regex="$1"
+shift
+INLINE_RULE='
+---
+id: replace-misc-include-regex
+language: cpp
+rule:
+  kind: function_declarator
+  inside:
+    kind: function_definition
+  regex: '$regex' 
+---
+id: replace-misc-include-regex
+language: c
+rule:
+  kind: function_declarator
+  inside:
+    kind: function_definition
+  regex: '$regex' 
+'
+
+ast-grep scan --json --inline-rules "$INLINE_RULE" "$@" |
+     jq -r '.[] | "\(.file):\(.range.start.line):\(.range.start.column): \(.text)"'
