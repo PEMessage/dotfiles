86d4d792320c62164055c2b365e5a6d4eec1bf2f
PEMessage <1165739182@qq.com>
2025-10-10 00:13:39 +0800

    q-task: add astgrep/rename-header.sh

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/astgrep/rename-header.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+from="$1"
+to="$2"
+
+shift 2
+
+if [ -z "$from" ] || [ -z "$to" ] ; then
+    echo "Usage: $(basename $0) FROM.h TO.h"
+fi
+
+INLINE_RULE='
+---
+id: rename-header
+language: c
+rule:
+  pattern: |
+    #include "'$from'"
+fix: |
+    #include "'$to'"
+---
+id: rename-header-cpp
+language: cpp
+rule:
+  pattern: |
+    #include "'$from'"
+fix: |
+    #include "'$to'"
+'
+
+ast-grep scan --inline-rules "$INLINE_RULE" "$@"
