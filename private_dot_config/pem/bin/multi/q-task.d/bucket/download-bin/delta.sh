2e280cb98316281699b13f41105a056cfacabfdd
PEMessage <1165739182@qq.com>
2025-11-04 00:48:02 +0800

    q-task: download-bin add delta.sh

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/download-bin/delta.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+runcmd() {
+    echo "Runing: $*"
+    "$@"
+}
+NAME=delta
+SUFFIX=.tar.gz
+
+DOWNLOAD=$NAME$SUFFIX
+
+if [ ! -f fd ] ; then
+    runcmd github-download dandavison/delta \
+        'delta-.*-x86_64-unknown-linux-musl.tar.gz' \
+        $NAME.tar.gz
+    filename="$(q-extract -l $DOWNLOAD | grep "/$NAME\$")"
+    runcmd q-extract -x "$DOWNLOAD" "$filename:$NAME"
+    runcmd rm -rf "$DOWNLOAD"
+fi
