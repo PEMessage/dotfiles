11d548b1f33f37909b00e877be3eadd952a3722c
PEMessage <1165739182@qq.com>
2025-12-15 00:16:27 +0800

    q-task: download-bin add nvim

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/download-bin/nvim-unsupport.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+runcmd() {
+    echo "Runing: $*"
+    "$@"
+}
+
+if [ ! -f nvim ] ; then
+    runcmd github-download neovim/neovim-releases \
+        'nvim-linux-x86_64.appimage$' \
+        nvim
+    chmod a+x nvim
+fi
