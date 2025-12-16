ca5b7410688fc99b6970d41d9785f046cb8c5ae6
PEMessage <1165739182@qq.com>
2025-12-17 00:12:03 +0800

    zsh: add parent_dir_tui, tweak behavior, alt-right set to `cd -<TAB>`

--- private_dot_config/pem/shell/better-cd/executable_parent_dir_tui.zsh
+++ private_dot_config/pem/shell/better-cd/executable_parent_dir_tui.zsh
@@ -13,5 +13,15 @@ parent_dir_tui_func() {
 }
 
 zle -N parent_dir_tui_func
-
 bindkey '^[[1;3D' parent_dir_tui_func
+
+
+insert-cd-back-and-complete() {
+    BUFFER="cd -"
+    CURSOR=$#BUFFER  # Move cursor to end
+    zle -R           # Refresh the line
+    zle expand-or-complete  # Trigger completion
+}
+
+zle -N insert-cd-back-and-complete
+bindkey '^[[1;3C' insert-cd-back-and-complete
