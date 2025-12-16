1fe9596c996c0116864030944d2ab69b6336bba3
PEMessage <1165739182@qq.com>
2025-12-17 00:36:11 +0800

    zsh: add parent_dir_tui, fix cmd not founded

--- private_dot_config/pem/shell/better-cd/executable_parent_dir_tui.zsh
+++ private_dot_config/pem/shell/better-cd/executable_parent_dir_tui.zsh
@@ -1,11 +1,12 @@
 
 parent_dir_tui_func() {
-    local selected_dir="$(fzy_parent_dir "$(pwd)")"
+    local selected_dir="$(parent_dir_tui "$(pwd)")"
 
     # Check if fzy_parent_dir succeeded and returned a non-empty directory
     if [[ $? -eq 0 && -n "$selected_dir" && -d "$selected_dir" ]]; then
-        cd  "${selected_dir}"
-        zle reset-prompt
+        cd "${selected_dir}"
+        echo "cd \"${selected_dir}\""
+        zle accept-line
     else
         zle reset-prompt
     fi
