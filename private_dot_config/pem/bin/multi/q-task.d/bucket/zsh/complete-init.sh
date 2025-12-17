2754e2329ccb2fa332380384bf009bbe90341cfe
PEMessage <1165739182@qq.com>
2025-12-17 23:54:14 +0800

    q-task: add zsh/complete-init.sh

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/zsh/complete-init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+runcmd() {
+    echo "Runing: $*"
+    "$@"
+}
+
+runeval() {
+    echo "Runing: $*"
+    eval "$*"
+}
+
+COMPLETE_PATH="$PEM_HOME/bin/extra/zsh.d"
+runcmd cd "$COMPLETE_PATH"
+
+if [ ! -f _chezmoi ] && command -v chezmoi &> /dev/null ; then
+    runeval 'chezmoi completion zsh > _chezmoi'
+fi
+
