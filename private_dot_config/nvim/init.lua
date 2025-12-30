147274c6d035154390e13c5ee9a0535d0f93657c
PEMessage <1165739182@qq.com>
2025-12-31 00:24:24 +0800

    nvim: using our version of flash.nvim, which support <TAB> <S-TAB> autoexit

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -667,7 +667,7 @@ require("lazy").setup({
     {
         'phaazon/hop.nvim',
         branch = 'v2', -- optional but strongly recommended
-        enabled = true,
+        enabled = false,
         event = "VeryLazy",
         opts = {},
         keys = {
@@ -676,11 +676,14 @@ require("lazy").setup({
         },
     },
     {
-        'folke/flash.nvim',
+        'PEMessage/flash.nvim',
         event = "VeryLazy",
-        enabled = false,
+        enabled = true,
         opts = function ()
-            vim.cmd [[ highlight! link FlashMatch IncSearch ]]
+            -- easymotion like highlight
+            vim.cmd [[ highlight! link FlashMatch String ]]
+            vim.cmd [[ highlight! link FlashCurrent ErrorMsg ]]
+            vim.cmd [[ highlight! link FlashLabel Search ]]
             return {
                 modes = {
                     -- Disable search integration. Better to use "s" if I want flash search.
@@ -689,11 +692,14 @@ require("lazy").setup({
                     search = {
                         enabled = false,
                     },
-                }
+                },
             }
         end,
+
         keys = {
-            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump({
+                jump = { autoexit = false }
+            }) end, desc = "Flash" },
             -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
             -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
             -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
