48562608529ceade9b2886cbda50e54305b50b64
PEMessage <1165739182@qq.com>
2026-01-18 22:07:56 +0800

    wezterm: improve rightclick behavior

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,6 +2,18 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 
 
+-- Thanks to: https://github.com/wezterm/wezterm/discussions/3541
+local function RightClick(window, pane)
+    local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+    if has_selection then
+        window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+        window:perform_action(act.ClearSelection, pane)
+    else
+        window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+    end
+
+end
+
 local config = {
     -- ==========================
     -- Color and style
@@ -26,48 +38,33 @@ local config = {
 
     mouse_bindings = {
 
-        --  Left + Shift
-        {
-            event={ Drag ={streak=1, button="Left"}},
-            mods="SHIFT",
-            action=wezterm.action{ExtendSelectionToMouseCursor = "Cell"},
-        },
+        --  LeftClick Down
         {
             event={ Down ={streak=1, button="Left"}},
             mods="SHIFT",
             action=wezterm.action{SelectTextAtMouseCursor = "Cell"},
         },
         {
+            event={ Drag ={streak=1, button="Left"}},
+            mods="SHIFT",
+            action=wezterm.action{ExtendSelectionToMouseCursor = "Cell"},
+        },
+        {
             event={ Up ={streak=1, button="Left"}},
             mods="SHIFT",
             action=wezterm.action{CompleteSelectionOrOpenLinkAtMouseCursor = "ClipboardAndPrimarySelection"},
         },
-        -- Right + Shift
+        -- RightClick
         {
             event={ Down ={streak=1, button="Right"}},
             mods="SHIFT",
-            action=wezterm.action{SelectTextAtMouseCursor = "Cell"},
+            action= wezterm.action_callback(RightClick)
         },
         {
-            event={ Up ={streak=1, button="Right"}},
-            mods="SHIFT",
-            action=wezterm.action{CompleteSelectionOrOpenLinkAtMouseCursor = "ClipboardAndPrimarySelection"},
-        },
-        -- Right Paste
-        {
-            -- Thanks to: https://github.com/wezterm/wezterm/discussions/3541
             event = { Down = { streak = 1, button = "Right" } },
             mods = "NONE",
-            action = wezterm.action_callback(function(window, pane)
-                local has_selection = window:get_selection_text_for_pane(pane) ~= ""
-                if has_selection then
-                    window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
-                    window:perform_action(act.ClearSelection, pane)
-                else
-                    window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
-                end
-            end),
-        },
+            action = wezterm.action_callback(RightClick)
+        }
     },
     launch_menu = {
         -- Default PowerShell (Windows)
