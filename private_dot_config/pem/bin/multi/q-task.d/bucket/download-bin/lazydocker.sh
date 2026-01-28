0c13fafd9ab12d385eea917172d2972b48d4b30e
PEMessage <1165739182@qq.com>
2026-01-29 00:53:11 +0800

    q-task: download-bin add lazydocker

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/download-bin/lazydocker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+runcmd() {
+    echo "Runing: $*"
+    "$@"
+}
+
+if [ ! -f lazydocker ] ; then
+    runcmd github-download jesseduffield/lazydocker \
+        'lazydocker_.*_Linux_x86_64.tar.gz' \
+        lazydocker.tar.gz
+    runcmd q-extract -x lazydocker.tar.gz lazydocker:lazydocker
+    runcmd rm -rf lazydocker.tar.gz
+fi
