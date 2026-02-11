a76b83afd3c18bbd4b8070609a2c55f7bebac627
PEMessage <1165739182@qq.com>
2026-02-12 00:37:06 +0800

    venv: add uvs to allow quickly run cmd in venv for inline-metadata py script(for pylsp)

---
+++ private_dot_config/pem/shell/venv/executable_uvs.sh
@@ -0,0 +1,31 @@
+
+# Expand from this oneline version
+# Thanks to: https://github.com/astral-sh/uv/issues/6637#issuecomment-2584434822
+# uv venv main.venv && uv export --script main.py | uv pip install -r - -p main.venv && uv run -p main.venv -- python -c "import anyio; print(anyio)"
+uvs() {
+    local script="$1"
+    shift
+    script_dir="$(dirname "$script")"
+    script_name="$(basename "$script" .py)"
+    script_env="$script_dir/.inline_meta_$script_name.venv"
+
+    if [ ! -f "$script" ] ; then
+        echo "[Err]: file not exist"
+        exit 1
+    fi
+
+    if [ ! -d "$script_env" ] ; then
+        uv venv "$script_env"
+        uv export --script "$script" |  uv pip install -r - -p "$script_env"
+    fi
+    (
+        . "$script_env/bin/activate" && "$@"
+    )
+}
+
+
+uvnvi() {
+    local script="$1"
+    shift
+    uvs "$script" nvim "$script" "$@"
+}
