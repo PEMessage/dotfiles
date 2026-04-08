97bcbebf0fab81fd0b6ba5606a38783586b3a144
PEMessage <1165739182@qq.com>
2026-04-09 00:38:12 +0800

    download-bin: add cli-proxy-api.sh

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/download-bin/cli-proxy-api.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+runcmd() {
+    echo "Runing: $*"
+    "$@"
+}
+
+if [ ! -f cli-proxy-api ] ; then
+    runcmd github-download router-for-me/CLIProxyAPI \
+        'CLIProxyAPI_.*_linux_amd64.tar.gz' \
+        cli-proxy-api.tar.gz
+    runcmd q-extract -x cli-proxy-api.tar.gz cli-proxy-api
+    runcmd rm -rf cli-proxy-api.tar.gz
+fi
