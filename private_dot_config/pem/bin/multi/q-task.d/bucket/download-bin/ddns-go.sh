6e70900fa0543235ed5ac474360a066942e4d2dc
PEMessage <1165739182@qq.com>
2026-04-14 23:23:54 +0800

    q-task: download-bin add ddns-go

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/download-bin/ddns-go.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+runcmd() {
+    echo "Runing: $*"
+    "$@"
+}
+
+if [ ! -f ddns-go ] ; then
+    runcmd github-download jeessy2/ddns-go \
+        'ddns-go_.*_linux_x86_64.tar.gz$' \
+        ddns-go.tar.gz
+    filename="$(q-extract -l ddns-go.tar.gz | grep 'ddns-go$')"
+    runcmd q-extract -x ddns-go.tar.gz "$filename:ddns-go"
+    runcmd rm -rf ddns-go.tar.gz
+fi
