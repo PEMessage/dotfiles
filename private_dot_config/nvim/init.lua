87070059f41d76440cda3c1f65c7c9fdab94f953
PEMessage <1165739182@qq.com>
2026-05-21 00:12:13 +0800

    nvim: using ts-comments.nvim, which just enhance nvim's native comments

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -855,8 +855,14 @@ require("lazy").setup({
     },
     {
         'numToStr/Comment.nvim',
-        event = "VeryLazy",
+        enabled = vim.fn.has("nvim-0.10.0") == 0,
         opts = {}
+    },
+    {
+        "folke/ts-comments.nvim",
+        enabled = vim.fn.has("nvim-0.10.0") == 1,
+        opts = {},
+        event = "VeryLazy",
     },
     {
         'kevinhwang91/nvim-bqf',
