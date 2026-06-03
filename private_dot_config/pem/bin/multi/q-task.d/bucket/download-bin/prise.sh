91ab635d74de9d20b96dd25effd830100480bd11
PEMessage <1165739182@qq.com>
2026-06-03 20:20:30 +0800

    q-task: add rockorager/prise

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/download-bin/prise.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+runcmd() {
+    echo "Runing: $*"
+    "$@"
+}
+
+if [ ! -f prise ] ; then
+    runcmd github-download rockorager/prise \
+        'prise-x86_64-linux.tar.gz' \
+        prise.tar.gz
+    runcmd q-extract -x prise.tar.gz prise
+    runcmd rm -rf prise.tar.gz
+fi
