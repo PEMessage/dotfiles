9052c337d7b733deefc8f489a705b806010e8f5b
PEMessage <1165739182@qq.com>
2026-06-15 00:55:19 +0800

    nvim: fix ]c/[c will switch quickfix even if we are not in qf buffer

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1366,8 +1366,9 @@ require("lazy").setup({
                 function()
                     if vim.wo.diff then
                         return '[c'
-                    elseif vim.api.nvim_get_option_value('filetype', { scope = 'local', buf = 0 }) == 'quickfix' or
-                        vim.iter(vim.fn.getwininfo()):find(function(w) return w.quickfix == 1 end) then
+                    elseif vim.bo.buftype == 'quickfix' then
+                        -- elseif vim.api.nvim_get_option_value('filetype', { scope = 'local', buf = 0 }) == 'quickfix' or
+                        --     vim.iter(vim.fn.getwininfo()):find(function(w) return w.quickfix == 1 end) then
                         return ':colder<CR>'
                     else
                         return '<cmd>Gitsigns prev_hunk<CR>'
@@ -1381,8 +1382,9 @@ require("lazy").setup({
                 function()
                     if vim.wo.diff then
                         return ']c'
-                    elseif vim.api.nvim_get_option_value('filetype', { scope = 'local', buf = 0 }) == 'quickfix' or
-                        vim.iter(vim.fn.getwininfo()):find(function(w) return w.quickfix == 1 end) then
+                    elseif vim.bo.buftype == 'quickfix' then
+                        -- elseif vim.api.nvim_get_option_value('filetype', { scope = 'local', buf = 0 }) == 'quickfix' or
+                        --     vim.iter(vim.fn.getwininfo()):find(function(w) return w.quickfix == 1 end) then
                         return ':cnewer<CR>'
                     else
                         return '<cmd>Gitsigns next_hunk<CR>'
@@ -3590,6 +3592,28 @@ require("lazy").setup({
     -- -------------------------------------------
     -- 5.10 AI
     -- -------------------------------------------
+    -- {
+    --     "nickjvandyke/opencode.nvim",
+    --     version = "*", -- Latest stable release
+    --     config = function()
+    --         ---@type opencode.Opts
+    --         vim.g.opencode_opts = {
+    --             -- Your configuration, if any; goto definition on the type or field for details
+    --         }
+    --
+    --         vim.o.autoread = true -- Required for `vim.g.opencode_opts.events.reload`
+    --
+    --         -- Recommended/example keymaps
+    --         vim.keymap.set({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ") end, { desc = "Ask opencode…" })
+    --         vim.keymap.set({ "n", "x" }, "<leader>os", function() require("opencode").select() end,       { desc = "Select opencode…" })
+    --
+    --         vim.keymap.set({ "n", "x" }, "go",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
+    --         vim.keymap.set("n",          "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
+    --
+    --         -- vim.keymap.set("n", "<C-M-u>", function() require("opencode").command("session.half.page.up") end,   { desc = "Scroll opencode up" })
+    --         -- vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end, { desc = "Scroll opencode down" })
+    --     end,
+    -- },
     -- {
     --     "yetone/avante.nvim",
     --     event = "VeryLazy",
