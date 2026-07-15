7a2a2e381bfb9e5a7f255c1c68d696fd77a7d882
PEMessage <1165739182@qq.com>
2026-07-16 00:18:16 +0800

    nvim: add wezterm-types

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -3148,13 +3148,19 @@ require("lazy").setup({
     {
         "folke/lazydev.nvim",
         ft = "lua", -- only load on lua files
+        dependencies = { 'DrKJeff16/wezterm-types' },
         opts = {
             library = {
                 -- See the configuration section for more details
                 -- Load luvit types when the `vim.uv` word is found
                 { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                { path = 'wezterm-types', mods = { 'wezterm' } },
             },
         },
+    },
+    {
+        'DrKJeff16/wezterm-types',
+        version = false, -- Get the latest version
     },
     -- -------------------------------------------
     -- 5.9 DAP Plug
