3a407a1dc5aa831c789fc2a57c2ffd1bf6bd9a8f
PEMessage <1165739182@qq.com>
2026-07-22 00:19:54 +0800

    q-task: download-bin add portview

---
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/download-bin/portview.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+runcmd() {
+    echo "Runing: $*"
+    "$@"
+}
+
+if [ ! -f portview ] ; then
+    runcmd github-download Mapika/portview \
+        'portview-linux-x86_64-musl.tar.gz' \
+        portview.tar.gz
+    filename="$(q-extract -l portview.tar.gz | grep '/portview$')"
+    runcmd q-extract -x portview.tar.gz "$filename:portview"
+    runcmd rm -rf portview.tar.gz
+fi
