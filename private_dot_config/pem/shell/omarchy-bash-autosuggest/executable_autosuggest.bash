5a3b3750679db75d617b90553751a559d9d0b876
PEMessage <1165739182@qq.com>
2026-09-14 02:17:39 +0800

    bash: autosuggest prevent enable dll twice

--- private_dot_config/pem/shell/omarchy-bash-autosuggest/executable_autosuggest.bash
+++ private_dot_config/pem/shell/omarchy-bash-autosuggest/executable_autosuggest.bash
@@ -5,7 +5,7 @@
 
 for _oba_so in "$HOME"/.local/lib/omarchy_autosuggest*.so; do
     [[ -r $_oba_so ]] || continue
-    omarchy_autosuggest status >/dev/null 2>&1 && continue
+    type omarchy_autosuggest &>/dev/null && continue
     enable -f "$_oba_so" omarchy_autosuggest 2>/dev/null
     omarchy_autosuggest enable 2>/dev/null
     omarchy_autosuggest limit 8192 2>/dev/null
