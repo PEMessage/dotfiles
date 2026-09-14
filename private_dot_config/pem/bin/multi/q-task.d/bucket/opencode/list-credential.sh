cc713c602885cf3bbb896705063194f960bc0390
PEMessage <1165739182@qq.com>
2026-09-14 23:59:17 +0800

    q-task: add opencode/list-credential.sh, better format if jq exist

--- private_dot_config/pem/bin/multi/q-task.d/bucket/opencode/list-credential.sh
+++ private_dot_config/pem/bin/multi/q-task.d/bucket/opencode/list-credential.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-sqlite3 "$(opencode debug paths | grep ^db | awk '{print $2}')" "SELECT * FROM credential;"
+! command -v sqlite3 > /dev/null && echo "sqlite3 not exist" && exit
+
+if command -v jq > /dev/null ; then
+    sqlite3 --json \
+        "$(opencode debug paths | grep ^db | awk '{print $2}')" \
+        "SELECT * FROM credential;" |
+        jq 'map(.value = (try (.value|fromjson) catch .value))'
+else
+    sqlite3 "$(opencode debug paths | grep ^db | awk '{print $2}')" "SELECT * FROM credential;"
+fi
+
